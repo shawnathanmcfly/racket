981a2f6c23ee45bcf5ea784855afad6a8f99fb44
shawnathanmcfly <sachimasi9187@gmail.com>
2019-03-07 20:08:16 -0500

    made walls wider

--- public/racket.c
+++ public/racket.c
@@ -3,25 +3,25 @@
 #include <stdlib.h>
 #include <SDL/SDL.h>
 
-#define PI   3.14159265358979323846264338327950288
+#define PI  3.14159265358979323846264338327950288
+#define CUBE_SIZE 200
 
 SDL_Window *window = 0;
 SDL_Renderer *renderer = 0;
 SDL_Surface *back = 0;
 
 unsigned short quit = 0;
-double player_dir = -1, player_x = 100, player_y = 100, rot = 0;
+double player_dir = -1, player_x = 300, player_y = 300, rot = 0;
 
-
-int map_width = 64 * 19, map_height = 64 * 18;
+int map_width = CUBE_SIZE * 19, map_height = CUBE_SIZE * 18;
 unsigned char level[19][18] = {
 
 	{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 1 },
 	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
+	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 1 },
 	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
-	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
-	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
-	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1 },
+	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 1, 1, 1, 1 },
 	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
 	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
 	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
@@ -46,20 +46,16 @@ double cast_ray( double offset ){
 	if( offset < 0 )
 		offset += 2 * 3.14;
 
-	if( !offset )
-		return 0;
-
 	//horizontal check
-	int step_y, step_x, y_start = floor(player_y / 64) * 64 - 1, x_start, h_check, v_check;
-	double hl, vl;
+	int step_y, step_x, y_start = floor(player_y / CUBE_SIZE) * CUBE_SIZE - 1, x_start, h_check, v_check;
+	double hl, vl, r = 0, g = 0, b = 0, hc, vc;
 	if( offset > 3.14 ){
-		step_y = -64;
-		step_x = 64 / tan(offset);
-		
+		step_y = -CUBE_SIZE;
+		step_x = CUBE_SIZE / tan(offset);	
 	}else{
-		step_y = 64;
-		step_x = -(64 / tan(offset));
-		y_start += 65;	
+		step_y = CUBE_SIZE;
+		step_x = -(CUBE_SIZE / tan(offset));
+		y_start += CUBE_SIZE + 1;
 	}
 
 	x_start = player_x - (player_y-y_start)/tan(offset);
@@ -73,28 +69,30 @@ double cast_ray( double offset ){
 
 	while( x_start > 0 && x_start < map_width &&
 			y_start > 0 && y_start < map_height &&
-	!level[y_start / 64][x_start / 64] ){
+	!level[y_start / CUBE_SIZE][x_start / CUBE_SIZE] ){
 
 		/*SDL_RenderDrawRect( renderer, &h );*/
 		v_check++;
 		x_start -= step_x;
 		y_start += step_y;
+		hc = level[y_start / CUBE_SIZE][x_start / CUBE_SIZE];
 		/*h.x = x_start;
 		h.y = y_start;*/
 	}
 
+	
 	hl = sqrt( (player_x - x_start) * (player_x - x_start) + (player_y - y_start) * (player_y - y_start) );
 
 	//vertical check
-	x_start = floor(player_x/64) * 64;
+	x_start = floor(player_x/CUBE_SIZE) * CUBE_SIZE;
 	if( offset < 4.71 && offset > 1.57 ){
-		step_x = 64;
-		step_y = -(64 * tan(offset));
+		step_x = CUBE_SIZE;
+		step_y = -(CUBE_SIZE * tan(offset));
 		x_start -= 1;
 	}else{
-		step_x = -64;
-		step_y = 64 * tan(offset);
-		x_start += 64;	
+		step_x = -CUBE_SIZE;
+		step_y = CUBE_SIZE * tan(offset);
+		x_start += CUBE_SIZE;
 	}
 
 	y_start = player_y - (player_x-x_start)*tan(offset);
@@ -108,22 +106,27 @@ double cast_ray( double offset ){
 	while( x_start > 0 && x_start < map_width &&
 			y_start > 0 && y_start < map_height &&
 	
-	!level[abs(y_start / 64)][abs(x_start / 64)] ){
+	!level[y_start / CUBE_SIZE][x_start / CUBE_SIZE] ){
 
 		//SDL_RenderDrawRect( renderer, &h );
 		x_start -= step_x;
 		y_start += step_y;
 		h_check++;
+		vc = level[y_start / CUBE_SIZE][x_start / CUBE_SIZE];
 		//h.x = x_start;
 		//h.y = y_start;
-
 	}
 
 	vl = sqrt( (player_x - x_start) * (player_x - x_start) + (player_y - y_start) * (player_y - y_start) );
 
-	if( vl < hl ){	
+	if( vl < hl ){
+		SDL_SetRenderDrawColor(renderer, 6000 / vl, 6000 / vl, 6000 / vl, 0xff);
+		vl *= cos( (offset - rot) + PI / 1080 );
 		return vl;
 	}else{
+		
+		SDL_SetRenderDrawColor(renderer, 6000 / hl, 6000 / hl, 6000 / hl, 0xff);
+		hl *= cos( (offset - rot) - PI / 1080 );
 		return hl;
 	}
 	/*if( vl < hl )
@@ -133,7 +136,7 @@ double cast_ray( double offset ){
 		SDL_RenderDrawLine(renderer, player_x, player_y, hx, hy);
 	}*/
 	
-	//printf( "VERTICAL: %d - HORIZONTAL: %d\n", vl, hl );	
+	//printf( "VERTICAL: %d - HORIZONTAL: %d\n", vl, hl );
 }
 
 int cast_rays( ){
@@ -145,30 +148,27 @@ int cast_rays( ){
 	cur_slice.x = slice;
 	cur_slice.w = 2;
 	
-	for( int i = 0; i < 160; i++, offset += 1 * 3.14 / 1080, slice += 2 ){
+	for( int i = 0; i < 160; i++, offset += 3.14 / 1080, slice += 2 ){
 		dist = cast_ray( rot + offset );
-		dist *= cos( offset - 1 * PI / 1080 );
 		cur_slice.x = slice;
 		cur_slice.h = 200 / floor( dist ) * 277;
 		cur_slice.y = 240 - cur_slice.h / 2;
-		SDL_SetRenderDrawColor(renderer, 8000 / dist, 8000 / dist, 8000 / dist, 0xff);
+		//SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
 		SDL_RenderFillRect(renderer, &cur_slice );
 	}
 
 	/*left check of view*/
-	offset = -1 * 3.14 / 1440;
+	offset = -1 * 3.14 / 1080;
 	slice = 640 / 2;
 	for( int i = 0; i < 160; i++, offset -= 1 * 3.14 / 1080, slice -= 2 ){
 
 		dist = cast_ray( rot + offset );
-		dist *= cos( offset + 1 * PI / 180 );
 		cur_slice.x = slice;
-		cur_slice.h = 200 / floor( dist ) * 277;
+		cur_slice.h = CUBE_SIZE / floor( dist ) * 277;
 		cur_slice.y = 240 - cur_slice.h / 2;
-		SDL_SetRenderDrawColor(renderer, 8000 / dist, 8000 / dist, 8000 / dist, 0xff);
+		//SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
 		SDL_RenderFillRect(renderer, &cur_slice );
 	}
-	
 }
 
 void main_loop(){
