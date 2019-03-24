c1f69816854191af652940b945347353c78c9437
shawnathanmcfly <shawn@archlinux.localdomain>
2019-03-23 23:09:51 -0400

    testing

--- public/racket.c
+++ public/racket.c
@@ -71,7 +71,7 @@ int main( int argc, char *argv[] ){
 
 	//
 	//point main loop function to emscripten
-	emscripten_set_main_loop( main_loop, 120, 1 );
+	emscripten_set_main_loop( main_loop, 30, 1 );
 
 	graphics_rem();
 
