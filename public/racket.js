fc6b5fbb364c46b2852b2d037e145cf66d108a60
shawnathanmcfly <shawn@archlinux.localdomain>
2019-04-12 23:36:09 -0400

    module load wait

--- public/racket.js
+++ public/racket.js
@@ -5,8 +5,9 @@ Module.onRuntimeInitialized = _ => {
 
     $("#game-window").append("<p class='logo'>Racket.</p>");
     $("#game-window").append("<p style='color:white;bottom:20px;left:20px;position:absolute;font-size:30px' class='start'>press SPACE to start</p>");
-    $(".appear").animate({top: "100px"}, 3000, function(){
+    $(".appear").animate({top: "100px"}, 4000, function(){
       this.remove();
+      Module._signal_module_ready();
     });
     $("#f-gui").prepend("<p align='center'>FRAGS</p><p align='center' style='font-size:40px;' id='frags'>" + 0 + "</p>");
     $("#f-gui").prepend("<p align='center'>HEALTH</p><p align='center' style='font-size:40px;' id='health'>" + 100 + "</p>");
